4e837fd050f8b0b9c0f2c2207ad528bb3c4e24f2
Tiago Pinto <up202206280@up.pt>
2023-11-30 12:30:38 +0000

    WRONG BRANCH

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -1,4 +1,20 @@
 package org.example.controller;
 
-public class Controller {
+import org.example.Game;
+import org.example.gui.Window;
+
+import java.io.IOException;
+
+public abstract class Controller<T> {
+    private final T model;
+
+    public Controller(T newModel) {
+        model = newModel;
+    }
+
+    public T getModel() {
+        return model;
+    }
+
+    public abstract void step(Game game, Window.KEY action, long time) throws IOException;
 }
--- src/main/java/org/example/states/State.java
+++ src/main/java/org/example/states/State.java
@@ -1,4 +1,32 @@
 package org.example.states;
 
-public class State {
+
+import org.example.Game;
+import org.example.controller.Controller;
+import org.example.gui.Window;
+import org.example.viewer.Viewer;
+
+import java.io.IOException;
+
+public abstract class State<T> {
+
+    private final T model;
+
+    public State(T newModel) {
+        this.model = newModel;
+    }
+
+    protected abstract Viewer<T> getViewer();
+
+    protected abstract Controller<T> getController();
+
+    public T getModel() {
+        return model;
+    }
+
+    public void step(Game game, Window window, long time) throws IOException {
+        Window.KEY action = window.processKey();
+        getController().step(game, action, time);
+        getViewer().draw(window);
+    }
 }
